4fc471c3b157591ee747a442f70f994934e538ea
XMLXSS <deaglorez@gmail.com>
2025-09-15 16:51:41 +0200

    initial

--- config.py
+++
@@ -1,6 +0,0 @@
-
-
-GITLAB_BASE_URL = "https://gitlab.com/api/v4"
-PROJECT_ID = "29866893"      # e.g. 123456 or "group%2Fproject"
-PRIVATE_TOKEN = "glpat-8NQphjVNm0Pz6NrG8KldfG86MQp1OmFueGNtCw.01.120xuwses"
-DATABASE_URI = "sqlite:///gitlab_mrs.db"
